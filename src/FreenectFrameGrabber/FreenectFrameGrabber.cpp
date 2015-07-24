3a4872938356ad9553eda41da786555926937df5
Ulrich Eck <ueck@net-labs.de>
2015-07-24 15:25:22 +0930

    copied from openni2 capture

---
+++ src/FreenectFrameGrabber/FreenectFrameGrabber.cpp
@@ -0,0 +1,249 @@
+/*
+ * Ubitrack - Library for Ubiquitous Tracking
+ * Copyright 2006, Technische Universitaet Muenchen, and individual
+ * contributors as indicated by the @authors tag. See the
+ * copyright.txt in the distribution for a full listing of individual
+ * contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+/**
+ * @ingroup vision_components
+ * @file
+ * Synchronouse capture of camera images using Point Grey's FlyCapture2 library.
+ *
+ * @author Ulrich Eck <ueck@net-labs.de>
+ *
+ */
+
+#include "FreenectFrameGrabber.h"
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <utDataflow/ComponentFactory.h>
+#include <utUtil/OS.h>
+#include <boost/array.hpp>
+
+#include <log4cpp/Category.hh>
+
+namespace Ubitrack { namespace Drivers {
+// get a logger
+static log4cpp::Category& logger( log4cpp::Category::getInstance( "Ubitrack.Vision.FreenectFrameGrabber" ) );
+
+
+using namespace Ubitrack;
+using namespace Ubitrack::Vision;
+using namespace Ubitrack::Drivers;
+using namespace openni;
+
+// static int to initialize/deinitialize openni only once.
+unsigned int FreenectModule::m_openni_initialized_count = 0;
+
+FreenectModule::FreenectModule( const FreenectModuleKey& moduleKey, boost::shared_ptr< Graph::UTQLSubgraph > subgraph, FactoryHelper* pFactory )
+        : Module< FreenectModuleKey, FreenectComponentKey, FreenectModule, FreenectComponent >( moduleKey, pFactory )
+		, m_device_url(m_moduleKey.get())
+		, m_timeout( 2000 ) // 2000ms
+        , m_bStop(false)
+{
+	if (m_openni_initialized_count == 0) {
+		Status rc = OpenNI::initialize();
+		if (rc != STATUS_OK)
+		{
+			LOG4CPP_ERROR( logger, "Freenect Initialize failed: " << OpenNI::getExtendedError() );
+			UBITRACK_THROW( "Freenect Initialize failed" );
+		}
+	}
+	m_openni_initialized_count++;
+}
+
+void FreenectModule::startModule() {
+
+	// start thread immediately - it will not send images if the module is not running ..
+	m_Thread.reset( new boost::thread( boost::bind( &FreenectModule::ThreadProc, this ) ) );
+
+}
+
+void FreenectModule::stopModule() {
+	// may need a lock here ...
+	if ( m_Thread )
+	{
+		m_bStop = true;
+		m_Thread->join();
+	}
+}
+
+
+FreenectModule::~FreenectModule()
+{
+	if (m_running) {
+		stopModule();
+	}
+
+	m_openni_initialized_count--;
+
+	if (m_openni_initialized_count == 0) {
+		OpenNI::shutdown();
+	}
+
+}
+
+
+void FreenectModule::ThreadProc()
+{
+	LOG4CPP_DEBUG( logger, "Freenect Thread started" );
+
+	const char* devurl = ANY_DEVICE;
+	if (!m_device_url.empty()) {
+		devurl = m_device_url.c_str();
+	}
+
+	Status rc = m_device.open(devurl);
+	if (rc != STATUS_OK)
+	{
+		LOG4CPP_ERROR( logger, "Couldn't open device: " << OpenNI::getExtendedError() );
+		return;
+	}
+
+
+	int index = 0;
+	ComponentList allComponents( getAllComponents() );
+	std::vector< VideoStream* > connected_streams;
+	std::vector< ComponentKey > connected_components;
+
+	for ( ComponentList::iterator it = allComponents.begin(); it != allComponents.end(); it++ ) {
+		if (m_device.getSensorInfo((*it)->getKey().getSensorType()) != NULL) {
+			VideoStream* stream = new VideoStream();
+
+			rc = stream->create(m_device, (*it)->getKey().getSensorType());
+			if (rc == STATUS_OK)
+			{
+				rc = stream->start();
+				if (rc != STATUS_OK)
+				{
+					LOG4CPP_ERROR( logger, "Couldn't start the stream" << std::endl << OpenNI::getExtendedError());
+					delete stream;
+					stream = NULL;
+				} else {
+					connected_streams.push_back(stream);
+					connected_components.push_back((*it)->getKey());
+				}
+			}
+			else
+			{
+				LOG4CPP_ERROR( logger, "Couldn't create the stream" << std::endl << OpenNI::getExtendedError());
+				delete stream;
+				stream = NULL;
+			}
+
+		} else {
+			LOG4CPP_WARN( logger, "Device has no sensor with type: " << (*it)->getKey().getSensorType());
+		}
+	}
+
+	VideoFrameRef frame;
+
+	while ( !m_bStop )
+	{
+		int readyStream = -1;
+		rc = OpenNI::waitForAnyStream(&connected_streams[0], connected_streams.size(), &readyStream, m_timeout);
+		Ubitrack::Measurement::Timestamp timestamp = Ubitrack::Measurement::now();
+
+		if (rc != STATUS_OK)
+		{
+			LOG4CPP_WARN( logger, "Wait failed! " << OpenNI::getExtendedError());
+			break;
+		}
+
+		if ((readyStream >= 0) && (readyStream < connected_streams.size())) {
+			connected_streams.at(readyStream)->readFrame(&frame);
+			getComponent( connected_components.at(readyStream) )->processImage(timestamp, frame);
+		} else {
+			LOG4CPP_WARN( logger, "Unknown stream ready for reading ...");
+		}
+	}
+
+	for (unsigned int i=0; i < connected_streams.size(); ++i) {
+		connected_streams.at(i)->stop();
+	}
+
+	m_device.close();
+	LOG4CPP_DEBUG( logger, "Freenect Thread stopped" );
+}
+
+boost::shared_ptr< FreenectModule::ComponentClass > FreenectModule::createComponent( const std::string&, const std::string& name, boost::shared_ptr< Graph::UTQLSubgraph> subgraph, const ComponentKey& key, ModuleClass* pModule ) {
+	return boost::shared_ptr< ComponentClass >( new FreenectComponent( name, subgraph, key, pModule ) );
+}
+
+
+FreenectComponent::FreenectComponent( const std::string& name, boost::shared_ptr< Graph::UTQLSubgraph > subgraph, const FreenectComponentKey& componentKey, FreenectModule* pModule )
+	: FreenectModule::Component( name, componentKey, pModule )
+	, m_outPort( "Output", *this )
+{
+
+}
+
+void FreenectComponent::processImage( Measurement::Timestamp ts, const openni::VideoFrameRef& frame) {
+	boost::shared_ptr< Vision::Image > pImage;
+
+	bool new_image_data = false;
+	switch (frame.getVideoMode().getPixelFormat())
+	{
+		case PIXEL_FORMAT_DEPTH_1_MM:
+		case PIXEL_FORMAT_DEPTH_100_UM:
+		case PIXEL_FORMAT_GRAY16:
+			pImage.reset(new Vision::Image(frame.getWidth(), frame.getHeight(), 1, IPL_DEPTH_16U));
+			pImage->origin = 0;
+			memcpy(pImage->imageData, (unsigned char*)frame.getData(), frame.getWidth() * frame.getHeight() * 1);
+			new_image_data = true;
+			break;
+
+
+		case PIXEL_FORMAT_RGB888:
+			pImage.reset(new Vision::Image(frame.getWidth(), frame.getHeight(), 3, IPL_DEPTH_8U));
+			pImage->origin = 0;
+			pImage->channelSeq[0] = 'R';
+			pImage->channelSeq[1] = 'G';
+			pImage->channelSeq[2] = 'B';
+			memcpy(pImage->imageData, (unsigned char*)frame.getData(), frame.getWidth() * frame.getHeight() * 3);
+			new_image_data = true;
+			break;
+		default:
+			LOG4CPP_WARN( logger, "Unknown pixel format: " << frame.getVideoMode().getPixelFormat());
+	}
+
+	// undistort and process here ..
+
+	if (new_image_data) {
+		m_outPort.send( Measurement::ImageMeasurement( ts, pImage ) );
+
+	}
+
+}
+
+std::ostream& operator<<( std::ostream& s, const FreenectComponentKey& k )
+{
+	s << "FreenectComponent[ " << k.getSensorType()  << " ]";
+	return s;
+}
+
+UBITRACK_REGISTER_COMPONENT( Dataflow::ComponentFactory* const cf ) {
+	cf->registerModule< FreenectModule > ( "FreenectFrameGrabber" );
+}
+
+} } // namespace Ubitrack::Drivers
---
+++ src/FreenectFrameGrabber/FreenectFrameGrabber.h
@@ -0,0 +1,222 @@
+/*
+ * Ubitrack - Library for Ubiquitous Tracking
+ * Copyright 2006, Technische Universitaet Muenchen, and individual
+ * contributors as indicated by the @authors tag. See the
+ * copyright.txt in the distribution for a full listing of individual
+ * contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+
+/**
+ * @ingroup driver_components
+ * @file
+ * Freenect driver
+ * This file contains the driver component to
+ * synchronously capture camera images using openni2.
+ *
+ * The received data is sent via a push interface.
+ *
+ * @author Ulrich Eck <ueck@net-labs.de>
+ */
+
+#ifndef __Freenect_h_INCLUDED__
+#define __Freenect_h_INCLUDED__
+
+
+#include <string>
+#include <cstdlib>
+
+#include <iostream>
+#include <map>
+#include <boost/array.hpp>
+#include <boost/utility.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/function.hpp>
+
+#include <utDataflow/PushSupplier.h>
+#include <utDataflow/PushConsumer.h>
+#include <utDataflow/Component.h>
+#include <utDataflow/Module.h>
+#include <utMeasurement/Measurement.h>
+#include <utMeasurement/TimestampSync.h>
+
+#include <utVision/Image.h>
+#include <opencv/cv.h>
+
+#include <OpenNI.h>
+
+
+
+
+namespace {
+	class FreenectSensorMap
+		: public std::map< std::string, openni::SensorType>
+	{
+	public:
+		FreenectSensorMap()
+		{
+			using namespace openni;
+			
+			(*this)[ "IR" ] = SENSOR_IR;
+			(*this)[ "COLOR" ] = SENSOR_COLOR;
+			(*this)[ "DEPTH" ] = SENSOR_DEPTH;
+		}
+	};
+	static FreenectSensorMap openni2SensorMap;
+
+	class FreenectPixelFormatMap 
+		: public std::map< std::string, openni::PixelFormat >
+	{
+	public:
+		FreenectPixelFormatMap()
+		{
+			using namespace openni;
+			
+			(*this)[ "DEPTH_1_MM" ] = PIXEL_FORMAT_DEPTH_1_MM;
+			(*this)[ "DEPTH_100_UM" ] = PIXEL_FORMAT_DEPTH_100_UM;
+			(*this)[ "SHIFT_9_2" ] = PIXEL_FORMAT_SHIFT_9_2;
+			(*this)[ "SHIFT_9_3" ] = PIXEL_FORMAT_SHIFT_9_3;
+			(*this)[ "RGB8" ] = PIXEL_FORMAT_RGB888;
+			(*this)[ "YUV422" ] = PIXEL_FORMAT_YUV422;
+			(*this)[ "GRAY8" ] = PIXEL_FORMAT_GRAY8;
+			(*this)[ "GRAY16" ] = PIXEL_FORMAT_GRAY16;
+			(*this)[ "JPEG" ] = PIXEL_FORMAT_JPEG;
+			(*this)[ "YUV" ] = PIXEL_FORMAT_YUYV;
+		}
+	};
+	static FreenectPixelFormatMap openni2PixelFormatMap;
+
+} // anonymous namespace
+
+
+
+namespace Ubitrack { namespace Drivers {
+using namespace Dataflow;
+
+// forward declaration
+class FreenectComponent;
+
+MAKE_NODEATTRIBUTEKEY_DEFAULT( FreenectModuleKey, std::string, "Camera", "deviceUrl", "" );
+
+/**
+ * Component key for openni2.
+ * Represents the camera
+ */
+class FreenectComponentKey
+{
+public:
+
+	FreenectComponentKey( boost::shared_ptr< Graph::UTQLSubgraph > subgraph )
+	: m_sensor_type( openni::SENSOR_DEPTH )
+	{
+
+		std::string sSensorType = subgraph->m_DataflowAttributes.getAttributeString( "sensorType" );
+		if ( openni2SensorMap.find( sSensorType ) == openni2SensorMap.end() )
+			UBITRACK_THROW( "unknown sensor type: \"" + sSensorType + "\"" );
+		m_sensor_type = openni2SensorMap[ sSensorType ];
+	}
+
+	// construct from sensor type
+	FreenectComponentKey( const openni::SensorType a )
+		: m_sensor_type( a )
+ 	{}
+	
+	openni::SensorType  getSensorType() const {
+		return m_sensor_type;
+	}
+
+	// less than operator for map
+	bool operator<( const FreenectComponentKey& b ) const
+    {
+		return m_sensor_type < b.m_sensor_type;
+    }
+
+protected:
+	openni::SensorType m_sensor_type;
+};
+
+
+
+/**
+ * Module for Freenect tracker.
+ * Does all the work
+ */
+class FreenectModule
+	: public Module< FreenectModuleKey, FreenectComponentKey, FreenectModule, FreenectComponent >
+{
+public:
+	/** UTQL constructor */
+	FreenectModule( const FreenectModuleKey& key, boost::shared_ptr< Graph::UTQLSubgraph >, FactoryHelper* pFactory );
+
+	/** destructor */
+	~FreenectModule();
+
+	virtual void startModule();
+
+	virtual void stopModule();
+
+protected:
+
+	// thread main loop
+	void ThreadProc();
+
+	// the thread
+	boost::scoped_ptr< boost::thread > m_Thread;
+
+	// stop the thread?
+	volatile bool m_bStop;
+
+	std::string m_device_url;
+
+	/** the device **/
+	openni::Device m_device;
+
+	/** create the components **/
+	boost::shared_ptr< ComponentClass > createComponent( const std::string&, const std::string& name, boost::shared_ptr< Graph::UTQLSubgraph> subgraph,
+														 const ComponentKey& key, ModuleClass* pModule );
+
+	unsigned int m_timeout;
+	static unsigned int m_openni_initialized_count;
+};
+
+std::ostream& operator<<( std::ostream& s, const FreenectComponentKey& k );
+
+/**
+ * Component for Freenect tracker.
+ */
+class FreenectComponent : public FreenectModule::Component {
+public:
+	/** constructor */
+	FreenectComponent( const std::string& name, boost::shared_ptr< Graph::UTQLSubgraph > subgraph, const FreenectComponentKey& componentKey, FreenectModule* pModule );
+
+	virtual void processImage( Measurement::Timestamp ts, const openni::VideoFrameRef& frame);
+
+	/** destructor */
+	~FreenectComponent() {};
+
+
+protected:
+
+
+	// the port
+	Dataflow::PushSupplier< Measurement::ImageMeasurement > m_outPort;
+};
+
+} } // namespace Ubitrack::Drivers
+
+#endif
